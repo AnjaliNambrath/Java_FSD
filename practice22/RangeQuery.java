0d74702a8b8e2fa01baeedc1cfc93840d4a9ce24
ANJALI NAMBRATH <41856006+AnjaliNambrath@users.noreply.github.com>
2022-07-28 13:07:27 +0530

    Update RangeQuery.java

--- practice22/RangeQuery.java
+++ practice22/RangeQuery.java
@@ -1,38 +1,27 @@
 package practice22;
 
+import java.util.*;
 public class RangeQuery
-{ 
-    static int k = 16;
-    static int N = 100000; 
-    static long table[][] = new long[N][k + 1]; 
-    static void buildSparseTable(int arr[], int n) 
-    { 
-        for (int i = 0; i < n; i++) 
-            table[i][0] = arr[i]; 
-        for (int j = 1; j <= k; j++) 
-            for (int i = 0; i <= n - (1 << j); i++) 
-                table[i][j] = table[i][j - 1] + table[i + (1 << (j - 1))][j - 1]; 
-    } 
-    static long query(int L, int R) 
-    {
-        long answer = 0; 
-        for (int j = k; j >= 0; j--)  
-        { 
-            if (L + (1 << j) - 1 <= R)  
-            { 
-                answer = answer + table[L][j];
-                L += 1 << j; 
-            } 
-        } 
-        return answer; 
-    }
-    public static void main(String args[]) 
-    { 
-        int arr[] = { 3, 7, 2, 5, 8, 9 }; 
-        int n = arr.length; 
-        buildSparseTable(arr, n); 
-        System.out.println(query(0, 5)); 
-        System.out.println(query(3, 5)); 
-        System.out.println(query(2, 4)); 
-    } 
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the length of array:");
+		int n=in.nextInt();
+		int a[]= new int[n];
+		System.out.println(("Enter elements:"));
+		for(int i=0;i<n;i++) {
+			a[i]=in.nextInt();
+		}
+		System.out.println("Enter the starting range:");
+		int L=in.nextInt();
+		System.out.println("Enter the ending range:");
+		int R=in.nextInt();
+		int sum=0;
+		for(int i=L;i<=R;i++) {
+			sum=sum+a[i];
+		}
+		System.out.println("Sum of Range:"+sum);
+		
+	}
 }
